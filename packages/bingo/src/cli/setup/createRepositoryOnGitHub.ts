09bc52a29ab1d20ee076ff1d7b1aabf539f7a41c
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-04 11:36:39 -0500

    fix(bingo): wait until repository labels exist for newly created repo

--- packages/bingo/src/cli/setup/createRepositoryOnGitHub.ts
+++ packages/bingo/src/cli/setup/createRepositoryOnGitHub.ts
@@ -28,4 +28,21 @@ export async function createRepositoryOnGitHub(
 			});
 		}
 	}
+
+	// GitHub asynchronously initializes default repo metadata such as labels.
+	// There's no built-in API to determine whether initialization is done,
+	// but we can approximate that by polling for whether labels are created.
+	// https://github.com/JoshuaKGoldberg/bingo/issues/243
+	// On the off chance the repository's organization has no default labels,
+	// we only retry up to 10 times.
+	for (let i = 0; i < 10; i += 1) {
+		const response = await octokit.request("GET /repos/{owner}/{repo}/labels", {
+			owner: target.owner,
+			repo: target.repository,
+		});
+
+		if (response.data.length) {
+			break;
+		}
+	}
 }
