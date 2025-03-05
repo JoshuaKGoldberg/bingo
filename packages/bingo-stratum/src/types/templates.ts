303ca16363eee1692a882c9772722010077ec4ea
Josh Goldberg <git@joshuakgoldberg.com>
2025-03-05 12:45:25 -0500

    fix: knip

--- packages/bingo-stratum/src/types/templates.ts
+++ packages/bingo-stratum/src/types/templates.ts
@@ -3,13 +3,11 @@ import {
 	InferredObject,
 	Template,
 	TemplateAbout,
-	TemplateContext,
 	TemplatePrepare,
 } from "bingo";
 import { z } from "zod";
 
 import { Base } from "./bases.js";
-import { BlockCreation } from "./creations.js";
 import { Preset } from "./presets.js";
 import { StratumRefinements } from "./refinements.js";
 
@@ -39,13 +37,6 @@ export interface StratumTemplateDefinition<OptionsShape extends AnyShape> {
 export interface StratumTemplateOptionsShape {
 	preset: z.ZodUnion<ZodPresetNameLiterals>;
 }
-
-export type StratumTemplateProduce<Options extends object> = (
-	context: TemplateContext<
-		Options & { preset: string },
-		StratumRefinements<Options>
-	>,
-) => Partial<BlockCreation<Options>>;
 
 export type ZodPresetNameLiterals = [
 	z.ZodLiteral<string>,
