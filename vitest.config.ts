790b1ca0377e996642e2ae87ce7299ef037a0257
Josh Goldberg <git@joshuakgoldberg.com>
2025-05-06 09:49:35 -0400

    chore: move vitest.workspace.ts settings into vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,17 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		coverage: {
-			exclude: [
-				"**/.*",
-				"**/*.d.ts",
-				"**/*.js",
-				"**/*.astro",
-				"**/vitest.*.ts",
-				"packages/*/src/index.ts",
-				"packages/bingo-fs/src/create*FileSystem.ts",
-				"packages/site/astro.config.ts",
-				"packages/site/src/content",
-			],
+			all: true,
+			exclude: ["**/.*/", "**/*.config.*", "**/*.d.ts"],
 		},
+		exclude: ["packages/*/lib"],
+		include: ["packages/*/src/**/*.ts"],
+		setupFiles: ["console-fail-test/setup"],
+		workspace: ["packages/*"],
 	},
 });
